6b8aea07492f2717ef121a49c239190cf220a0cc
i-Raush <kumarraushanraj.it19@aryacollege.in>
2023-05-04 22:50:05 +0530

    commit

--- summarizer/src/components/Footer/Footer.jsx
+++ summarizer/src/components/Footer/Footer.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import Logo from '../../assets/logo.png';
+import './footer.css';
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='logo'>
+        <img src={Logo} alt="logo" />
+      </div>
+
+      <div className='menu_items'>
+        <ul className="nav_items_list">
+          <li>Services</li>
+          <li>Contact</li>
+          <li>Support</li>
+        </ul>
+        <ul className="nav_items_list">
+          <li>Premium Summarizer</li>
+          <li>Linkedin</li>
+          <li>How it Works</li>
+        </ul>
+        <ul className="nav_items_list">
+          <li>Free Summarizer</li>
+          <li>Instagram</li>
+          <li>F&Q</li>
+        </ul>
+        <ul className="nav_items_list">
+          <li>Pay Per use Summarizer</li>
+          <li>Twitter</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
