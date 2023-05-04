b38ff6be83b11a216ad2a6ea58129c738b121247
i-Raush <kumarraushanraj.it19@aryacollege.in>
2023-05-04 23:30:36 +0530

    footer commit 1st

--- summarizer/src/components/Footer/Footer.jsx
+++ summarizer/src/components/Footer/Footer.jsx
@@ -5,32 +5,33 @@ import './footer.css';
 function Footer() {
   return (
     <div className='footer'>
-      <div className='logo'>
+      <div className='logo1'>
         <img src={Logo} alt="logo" />
       </div>
 
-      <div className='menu_items'>
-        <ul className="nav_items_list">
+      <div className='menu_items_main'>
+        <ul className="nav_items_list1">
           <li>Services</li>
           <li>Contact</li>
           <li>Support</li>
         </ul>
-        <ul className="nav_items_list">
+        <ul className="nav_items_list2">
           <li>Premium Summarizer</li>
           <li>Linkedin</li>
           <li>How it Works</li>
         </ul>
-        <ul className="nav_items_list">
+        <ul className="nav_items_list3">
           <li>Free Summarizer</li>
           <li>Instagram</li>
           <li>F&Q</li>
         </ul>
-        <ul className="nav_items_list">
+        <ul className="nav_items_list4">
           <li>Pay Per use Summarizer</li>
           <li>Twitter</li>
           <li>Contact Us</li>
         </ul>
       </div>
+      <p>Copyright &c</p>
     </div>
   )
 }
