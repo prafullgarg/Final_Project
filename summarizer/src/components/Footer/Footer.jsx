1c1a4ed9367bd0147a4ce66dc8e4b2d5c7327aa4
PrafullGarg_11 <gargprafull55@gmail.com>
2023-05-05 09:53:00 +0530

    footer added

--- summarizer/src/components/Footer/Footer.jsx
+++ summarizer/src/components/Footer/Footer.jsx
@@ -4,36 +4,37 @@ import './footer.css';
 
 function Footer() {
   return (
+    <div className="main">
+
     <div className='footer'>
+      <div  className='name'>
       <div className='logo1'>
         <img src={Logo} alt="logo" />
       </div>
+        <h3>Legal Summarizer</h3>
+      </div>
 
       <div className='menu_items_main'>
-        <ul className="nav_items_list1">
-          <li>Services</li>
-          <li>Contact</li>
-          <li>Support</li>
-        </ul>
         <ul className="nav_items_list2">
+          <li><h2>Services</h2></li>
           <li>Premium Summarizer</li>
           <li>Linkedin</li>
           <li>How it Works</li>
         </ul>
         <ul className="nav_items_list3">
+        <li><h2>Contact</h2></li>
           <li>Free Summarizer</li>
           <li>Instagram</li>
           <li>F&Q</li>
         </ul>
         <ul className="nav_items_list4">
+        <li><h2>Support</h2></li>
           <li>Pay Per use Summarizer</li>
           <li>Twitter</li>
           <li>Contact Us</li>
         </ul>
       </div>
-      <div  className='name'>
-        <p>Legal Summarizer</p>
-      </div>
+    </div>
       <div className='copyright'>
         <p>Copyright &copy; By LegalSummarizer.com | All Rights Reserved</p>
       </div>
--- summarizer/src/components/Navbar/Navbar.jsx
+++ summarizer/src/components/Navbar/Navbar.jsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import Logo from '../../assets/logo.png';
-import './navbar.css';
+import React from "react";
+import Logo from "../../assets/logo.png";
+import "./navbar.css";
 
 function Navbar() {
   return (
-    <div className='navbar'>
-      <div className='logo'>
-      <a href="/"><img src={Logo} alt="logo" /></a>
+    <div className="navbar">
+      <div className="logo_container">
+        <div className="logo">
+          <a href="/">
+            <img src={Logo} alt="logo" />
+          </a>
+        </div>
+          <h3>Legal Summarizer</h3>
       </div>
 
-      <div className='menu_items'>
+      <div className="menu_items">
         <ul className="nav_items_list">
-          <li> <a href="#summarizer"> Summarizer </a></li>
+          <li>
+            {" "}
+            <a href="#summarizer"> Summarizer </a>
+          </li>
           <li>Pricing</li>
           <li>About us</li>
           <li>Blogs</li>
-          <li><button><a href="/">SignIn/SignUp</a></button></li>
+          <li>
+            <button>
+              <a href="/">SignIn/SignUp</a>
+            </button>
+          </li>
         </ul>
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
